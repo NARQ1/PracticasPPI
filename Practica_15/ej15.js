6a22a2676e2baa7eb483d020a60e5d948a860d16
NARQ1 <nestor.rivera3073@alumnos.udg.mx>
2026-03-27 21:22:49 -0600

    Fin practica 15
    Se termino la practica 15

--- Practica_15/ej15.js
+++ Practica_15/ej15.js
@@ -64,7 +64,7 @@ const inventario = [
 //printData(inventario[0])='El producto 1K de tortilla de costo $20 tiene un id 1'
 //printData(inventario[3])='El producto 1L Leche sello rojo de costo $25 tiene un id 4'
 //printData(inventario[6])='El producto Lata de chiles jalapeños la costeña de costo $12 tiene un id 7'
-function printData(/*Agregar código*/) {
+function printData({name: miproducto, price: costo, id:identificador}) {
 	return `El producto ${miproducto} de costo $${costo} tiene un id ${identificador}`;
 }
 
@@ -80,8 +80,8 @@ El producto CSTCHJ tiene un stock de 12/12
 */
 //Revisar que no tenga datos undefined, el for a usar es aquel que sirve con objetos.
 function printTodo(inventory) {
-	for (/*Agregar código*/) {
-		/*Agregar código*/
+	for (let producto of inventario) {
+		const{name: nombre="Sin nombre",stock: disp=0,maxStock: max=0}=producto;
 		console.log(`El producto ${nombre} tiene un stock de ${disp}/${max}`);
 	}
 }
